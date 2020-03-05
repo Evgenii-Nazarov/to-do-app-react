b843a2a6ae7c7c2db12b8eabe7acf110354f7045
Evgenii Nazarov <ev66647260@gmail.com>
2020-03-04 23:48:37 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const [noteList, setNoteList] = useState ([
+        {noteName:'Note 1', _id: '123'},
+        {noteName:'Note 2', _id: '234'},
+        {noteName:'Note 3', _id: '345'},
+        ]
+    );
+
+    const [newNote, setNewNote] = useState('');
+
+    const addNewNoteHandler = (e) => {
+        setNewNote(e.target.value)
+    };
+
+    const addNoteButtonHandler = () => {
+        const newNoteList = [...noteList,
+            { noteName: newNote,
+                _id: Math.random()
+            }];
+        setNoteList(newNoteList);
+        setNewNote('');
+    };
+
+    const deleteNoteButtonHandler = _id => {
+         const newNoteList = noteList.filter(el => el._id !== _id);
+         setNoteList(newNoteList);
+    };
+
+    const completeNoteButtonHandler = _id => {
+
+    };
+
+    return (
+        <div className="container">
+            <ul>
+            {
+                noteList.map(el => <div key={el._id}>
+
+                    <li> {el.noteName}</li>
+                    <Button variant="outline-primary" onClick={()=>completeNoteButtonHandler(el._id)}>complete</Button>
+                    <Button onClick={()=>deleteNoteButtonHandler(el._id)}>delete</Button>
+                    </div>
+                )
+            }
+            </ul>
+            <input type="text" value={newNote}  placeholder='input new note' onChange={addNewNoteHandler}/>
+            <button onClick={addNoteButtonHandler}>add</button>
+        </div>
   );
-}
+};
 
 export default App;
