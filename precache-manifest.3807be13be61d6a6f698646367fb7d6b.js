7b9b3ae848ce47e26cd39e3c196c8c65555cdb35
Evgenii Nazarov <ev66647260@gmail.com>
2020-03-07 00:25:45 -0800

    Updates

---
+++ precache-manifest.3807be13be61d6a6f698646367fb7d6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e0c21f8133d86fef500bd267195831",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "f0c586c9cd7a7e44e439",
+    "url": "/to-do-app-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "57f8d3db945677e31f55",
+    "url": "/to-do-app-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f0c586c9cd7a7e44e439",
+    "url": "/to-do-app-react/static/js/2.44810649.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/to-do-app-react/static/js/2.44810649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f8d3db945677e31f55",
+    "url": "/to-do-app-react/static/js/main.5497a8ee.chunk.js"
+  },
+  {
+    "revision": "c49d1922962058d47995",
+    "url": "/to-do-app-react/static/js/runtime-main.6513c9ec.js"
+  }
+]);
